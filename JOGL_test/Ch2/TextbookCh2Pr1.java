1e8d9af0b0fd7ad09671c1e66416cdcb1330e729
zazyzaya <zazyzaya@gmail.com>
2019-08-27 16:06:14 -0400

    Working on ch2

--- JOGL_test/Ch2/TextbookCh2Pr1.java
+++ JOGL_test/Ch2/TextbookCh2Pr1.java
@@ -5,11 +5,11 @@ import com.jogamp.opengl.*;
 import com.jogamp.opengl.awt.GLCanvas;
 import com.jogamp.common.nio.Buffers;
 
-public class JOGL_Test extends JFrame implements GLEventListener
+public class TextbookCh2Pr1 extends JFrame implements GLEventListener
 	{ 
 	private GLCanvas myCanvas;
 	
-	public JOGL_Test()
+	public TextbookCh2Pr1()
 	{ 
 		setTitle("Chapter2 - program1");
 		setSize(600, 400);
@@ -17,20 +17,30 @@ public class JOGL_Test extends JFrame implements GLEventListener
 		myCanvas = new GLCanvas();
 		myCanvas.addGLEventListener(this);
 		this.add(myCanvas);
-		setVisible(true);
+		setVisible(true); // This initializes OpenGL and creates a GL4 object
 	}
 	
+	// The OpenGL object created in the constructor calls init first, and passes it myCanvas
+	// Then, the OpenGL object calls display
 	public void display(GLAutoDrawable drawable)
 	{ 
 		GL4 gl = (GL4) GLContext.getCurrentGL();
+		
+		// The C OpenGL function for glClearBuffer expects a pointer; Java does not have pointers
+		// However, we can use a floatbuffer object instead, which essentially is a pointer to bkg
+		// Note that color is a 4D vector for RGB and A
 		float bkg[] = { 1.0f, 0.0f, 0.0f, 1.0f };
 		FloatBuffer bkgBuffer = Buffers.newDirectFloatBuffer(bkg);
+		
+		// We use the GL4 interface to make calls to the underlying C OpenGL library
+		// This call tells OpenGL we are putting a color buffer into it's 0th slot for such buffers
+		// The final param is the pointer to the data that we are loading in
 		gl.glClearBufferfv(GL_COLOR, 0, bkgBuffer);
 	}
 	
 	public static void main(String[ ] args)
 	{ 
-		new JOGL_Test();
+		new TextbookCh2Pr1();
 	}
 	
 	public void init(GLAutoDrawable drawable) { }
